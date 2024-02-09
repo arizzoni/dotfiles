c24f3bb09001afd7489ee428df617ab18b9436a1
Alex Rizzoni <rizzoni.alex@gmail.com>
2024-02-09 13:33:50 -0500

    added automatic linter management

---
+++ .config/nvim/lua/plugins/mason-nvim-lint/init.lua
@@ -0,0 +1,25 @@
+-- Autocompletion
+return {
+  "rshkarin/mason-nvim-lint",
+  name = "mason-nvim-lint",
+  dependencies = {
+    "williamboman/mason.nvim", -- Snippet Engine
+    "mfussenegger/nvim-lint",  -- Snippet Engine
+  },
+  opts = {
+    ensure_installed = {
+      "cmakelint",
+      "cpplint",
+      "jsonlint",
+      "mypy",
+      "proselint",
+      "pylint",
+      "selene",
+      "shellcheck",
+      "textlint",
+      "yamllint",
+    },
+    automatic_installation = true,
+    handlers = {},
+  }
+}
