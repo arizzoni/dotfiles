e1adbbf11b937459b31dedb3384aad7863899682
Alex Rizzoni <rizzoni.alex@gmail.com>
2024-02-10 00:28:39 -0500

    added terminal_colors

--- .config/nvim/colors/patched-lushwal.lua
+++ .config/nvim/colors/patched-lushwal.lua
@@ -1,9 +1,31 @@
 -- First we will need lush, and the colorscheme we wish to modify
 local lush = require("lush")
 local lushwal = require("lushwal.base")
+local get_colors = require("lushwal.colors")
 
 vim.o.background = "dark"
 vim.g.colorscheme_name = "patched-lushwal"
+
+local colors = get_colors()
+
+if colors ~= nil then
+  vim.g.terminal_color_0 = colors.color0
+  vim.g.terminal_color_1 = colors.color1
+  vim.g.terminal_color_2 = colors.color2
+  vim.g.terminal_color_3 = colors.color3
+  vim.g.terminal_color_4 = colors.color4
+  vim.g.terminal_color_5 = colors.color5
+  vim.g.terminal_color_6 = colors.color6
+  vim.g.terminal_color_7 = colors.color7
+  vim.g.terminal_color_8 = colors.color8
+  vim.g.terminal_color_9 = colors.color9
+  vim.g.terminal_color_10 = colors.color10
+  vim.g.terminal_color_11 = colors.color11
+  vim.g.terminal_color_12 = colors.color12
+  vim.g.terminal_color_13 = colors.color13
+  vim.g.terminal_color_14 = colors.color14
+  vim.g.terminal_color_15 = colors.color15
+end
 
 -- we can apply modifications ontop of the existing colorscheme
 local spec = lush.extends({ lushwal }).with(function()
@@ -19,5 +41,3 @@ end)
 
 -- then pass the extended spec to lush for application
 lush(spec)
-
--- vim: tabstop=2 shiftwidth=2 expandtab
