88dd52bbbd81a38ec3086e516ae1f04b0c584928
Alex Rizzoni <rizzoni.alex@gmail.com>
2024-02-14 01:03:07 -0500

    changed scale of 4k monitor

--- bin/monitors.bash
+++ bin/monitors.bash
@@ -1,4 +1,4 @@
 #!/bin/sh
-xrandr --output eDP-1 --mode 1920x1200 --pos 960x2160 --rotate normal --output DP-1 --primary --mode 3840x2160 --pos 0x0 --rotate normal
+xrandr --output eDP-1 --mode 1920x1200 --pos 480x2160 --rotate normal --output DP-1 --primary --mode 3840x2160 --pos 0x0 --rotate normal --scale 0.75
 xbacklight -set 10
 
