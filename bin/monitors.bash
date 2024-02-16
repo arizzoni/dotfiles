ed52abe982ac586fa387fb887fd67a3bb2044ef8
Alex Rizzoni <rizzoni.alex@gmail.com>
2024-02-15 23:48:38 -0500

    specified refresh rate

--- bin/monitors.bash
+++ bin/monitors.bash
@@ -1,4 +1,4 @@
 #!/bin/sh
-xrandr --output eDP-1 --mode 1920x1200 --pos 480x2160 --rotate normal --output DP-1 --primary --mode 3840x2160 --pos 0x0 --rotate normal --scale 0.75
+xrandr --output eDP-1 --mode 1920x1200 --pos 480x2160 --rotate normal --rate 165 --output DP-1 --primary --mode 3840x2160 --pos 0x0 --rotate normal --rate 60
 xbacklight -set 10
 
