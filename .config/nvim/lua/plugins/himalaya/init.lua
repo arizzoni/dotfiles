0e4d25f4c07c5a8e992e7a4f7445ebebfd84120e
Alex Rizzoni <rizzoni.alex@gmail.com>
2024-02-18 17:09:02 -0500

    removed himalaya in favor of aerc

--- .config/nvim/lua/plugins/himalaya/init.lua
+++
@@ -1,8 +0,0 @@
-return {
-  url = "https://git.sr.ht/~soywod/himalaya-vim",
-  config = function()
-    vim.g.himalaya_executable = "/usr/bin/himalaya"
-    vim.g.himalaya_folder_picker = "telescope"
-    vim.g.himalaya_folder_picker_telescope_preview = 1
-  end
-}
