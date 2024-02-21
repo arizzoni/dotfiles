7fe2b2f8d73e5bf4262dda21c511639fba1a193b
Alex Rizzoni <rizzoni.alex@gmail.com>
2024-02-21 16:26:22 -0500

    moved split function to utils

---
+++ .config/awesome/util/init.lua
@@ -0,0 +1,14 @@
+local util = {}
+
+function util.split(inputstr, sep)
+  if sep == nil then
+    sep = "%s"
+  end
+  local t = {}
+  for str in string.gmatch(inputstr, "([^" .. sep .. "]+)") do
+    table.insert(t, str)
+  end
+  return t
+end
+
+return util
