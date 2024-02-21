dac2b2b56389a7c332d7e1e8c64ce2178d80b28c
Alex Rizzoni <rizzoni.alex@gmail.com>
2024-02-21 00:06:06 -0500

    set up widgets to return modules

--- .config/awesome/widgets/battery.lua
+++ .config/awesome/widgets/battery.lua
@@ -1,74 +1,78 @@
+local M = {}
 
-  -- Create a battery widget
+local awful = require("awful")
+-- Create a battery widget
 
-  -- Discharging                󰁹 󰂂 󰂁 󰂀 󰁿 󰁾 󰁽 󰁻 󰁺 󰂎
-  -- Discharging Status         󰂃 󰂑 󰂄 󰂏 󰂌 󱈑 󱟞 󱟟 󱟠 󱟡 󱟢 󱟣 󱟤 󱟥 󱟦 󱟧 󱟨 󱟩 󱧥 󱧦
-  -- Wired Charging             󰂆 󰂇 󰂈 󰂉 󰂊 󰂋 󰂅
-  -- Wired Charging Status      󰂍 󰂐 󱈏 󱈐
+-- Discharging                󰁹 󰂂 󰂁 󰂀 󰁿 󰁾 󰁽 󰁻 󰁺 󰂎
+-- Discharging Status         󰂃 󰂑 󰂄 󰂏 󰂌 󱈑 󱟞 󱟟 󱟠 󱟡 󱟢 󱟣 󱟤 󱟥 󱟦 󱟧 󱟨 󱟩 󱧥 󱧦
+-- Wired Charging             󰂆 󰂇 󰂈 󰂉 󰂊 󰂋 󰂅
+-- Wired Charging Status      󰂍 󰂐 󱈏 󱈐
 
-  -- Not Implemented:
-  -- Wireless Charging          󰠒 󰠈 󰠉 󰠊 󰠋 󰠌 󰠍 󰠎 󰠏 󰠐 󰠇
-  -- Wireless Charging Status   󰠑
+-- Not Implemented:
+-- Wireless Charging          󰠒 󰠈 󰠉 󰠊 󰠋 󰠌 󰠍 󰠎 󰠏 󰠐 󰠇
+-- Wireless Charging Status   󰠑
 
-  -- TODO: add status icons for battery health
-  -- TODO: add support for multiple batteries
-  -- TODO: Dropdown Menu
-  s.mybattery = awful.widget.watch("acpi -V", 1,
-    function(widget, stdout)
-      local battery_icon = ""
-      local words = {}
-      local charging = true
+-- TODO: add status icons for battery health
+-- TODO: add support for multiple batteries
+-- TODO: Dropdown Menu
+M.mybattery = awful.widget.watch("acpi -V", 1,
+  function(widget, stdout)
+    local battery_icon = ""
+    local words = {}
+    local charging = true
 
-      for line in stdout:gmatch("[^\r\n]+") do
-        if line:match("Battery 0: Discharging") then
-          charging = false
-        end
-        for word in line:gmatch("%S+") do
-          table.insert(words, word)
-        end
+    for line in stdout:gmatch("[^\r\n]+") do
+      if line:match("Battery 0: Discharging") then
+        charging = false
       end
-
-      local charge = tonumber(string.match(words[4], "%d+"))
-
-      if charging == true then
-        if charge <= 100 and charge > 90 then
-          battery_icon = "󰂅 "
-        elseif charge <= 90 and charge > 80 then
-          battery_icon = "󰂋 "
-        elseif charge <= 80 and charge > 70 then
-          battery_icon = "󰂊 "
-        elseif charge <= 70 and charge > 60 then
-          battery_icon = "󰂉 "
-        elseif charge <= 60 and charge > 50 then
-          battery_icon = "󰂈 "
-        elseif charge <= 50 and charge > 40 then
-          battery_icon = "󰂇 "
-        elseif charge <= 40 and charge > 0 then
-          battery_icon = "󰂆 "
-        end
-      elseif charging == false then
-        if charge <= 100 and charge > 90 then
-          battery_icon = "󰁹 "
-        elseif charge <= 90 and charge > 80 then
-          battery_icon = "󰂂 "
-        elseif charge <= 80 and charge > 70 then
-          battery_icon = "󰂁 "
-        elseif charge <= 70 and charge > 60 then
-          battery_icon = "󰂀 "
-        elseif charge <= 60 and charge > 50 then
-          battery_icon = "󰁿 "
-        elseif charge <= 50 and charge > 40 then
-          battery_icon = "󰁾 "
-        elseif charge <= 40 and charge > 30 then
-          battery_icon = "󰁽 "
-        elseif charge <= 30 and charge > 20 then
-          battery_icon = "󰁻 "
-        elseif charge <= 20 and charge > 10 then
-          battery_icon = "󰁺 "
-        elseif charge <= 10 and charge > 0 then
-          battery_icon = "󰂎 "
-        end
+      for word in line:gmatch("%S+") do
+        table.insert(words, word)
       end
-      widget:set_text(battery_icon .. tostring(charge) .. "%" .. " ")
     end
-  )
+
+    local charge = tonumber(string.match(words[4], "%d+"))
+
+    if charging == true then
+      if charge <= 100 and charge > 90 then
+        battery_icon = "󰂅 "
+      elseif charge <= 90 and charge > 80 then
+        battery_icon = "󰂋 "
+      elseif charge <= 80 and charge > 70 then
+        battery_icon = "󰂊 "
+      elseif charge <= 70 and charge > 60 then
+        battery_icon = "󰂉 "
+      elseif charge <= 60 and charge > 50 then
+        battery_icon = "󰂈 "
+      elseif charge <= 50 and charge > 40 then
+        battery_icon = "󰂇 "
+      elseif charge <= 40 and charge > 0 then
+        battery_icon = "󰂆 "
+      end
+    elseif charging == false then
+      if charge <= 100 and charge > 90 then
+        battery_icon = "󰁹 "
+      elseif charge <= 90 and charge > 80 then
+        battery_icon = "󰂂 "
+      elseif charge <= 80 and charge > 70 then
+        battery_icon = "󰂁 "
+      elseif charge <= 70 and charge > 60 then
+        battery_icon = "󰂀 "
+      elseif charge <= 60 and charge > 50 then
+        battery_icon = "󰁿 "
+      elseif charge <= 50 and charge > 40 then
+        battery_icon = "󰁾 "
+      elseif charge <= 40 and charge > 30 then
+        battery_icon = "󰁽 "
+      elseif charge <= 30 and charge > 20 then
+        battery_icon = "󰁻 "
+      elseif charge <= 20 and charge > 10 then
+        battery_icon = "󰁺 "
+      elseif charge <= 10 and charge > 0 then
+        battery_icon = "󰂎 "
+      end
+    end
+    widget:set_text(battery_icon .. tostring(charge) .. "%" .. " ")
+  end
+)
+
+return M
