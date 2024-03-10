bbb2792c681c856436dc6701107ec16d7090cb56
Alex Rizzoni <rizzoni.alex@gmail.com>
2024-03-09 23:09:03 -0500

    set backlight

--- bin/monitors.bash
+++ bin/monitors.bash
@@ -28,3 +28,5 @@ elif [ -n "$DP_1" ] && [ -n "$eDP_1" ] ; then {
 else {
     xrandr --output DP-0 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-0 --off --output DP-2 --off --output None-2-1 --off --output eDP-1-1 --off --output DP-1-1 --primary --mode 2560x1440 --pos 2560x0 --rotate normal --output DP-1-2 --off --output DP-1-3 --off --output DP-1-4 --off
 } fi
+
+xbacklight -set 1
