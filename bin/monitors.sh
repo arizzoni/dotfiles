6855aa8d7f6df47bdb91b37bea66221c342daf4c
Alex Rizzoni <rizzoni.alex@gmail.com>
2024-03-16 00:08:07 -0400

    ported to posix shell

--- bin/monitors.sh
+++ bin/monitors.sh
@@ -24,13 +24,13 @@ DP_1_1="$($CONNECTED_MONITORS | grep DP-1-1)"
 
 
 if [ -n "$DP_0" ] && [ -n "$DP_1_1" ] && [ -n "$eDP_1_1" ] ; then {
-    xrandr --output DP-0 --mode 2560x1440 --rate 60 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-0 --off --output DP-2 --off --output None-2-1 --off --output eDP-1-1 --mode 1920x1200 --rate 165 --pos 2799x1440 --rotate normal --output DP-1-1 --primary --mode 2560x1440 --rate 60 --pos 2560x0 --rotate normal --output DP-1-2 --off --output DP-1-3 --off --output DP-1-4 --off
-}
-elif [ -n "$DP_1" ] && [ -n "$eDP_1" ] ; then {
-    xrandr --output eDP-1 --mode 1920x1200 --pos 480x2160 --rotate normal --rate 165 --output DP-1 --primary --mode 3840x2160 --pos 0x0 --rotate normal --rate 60
-} 
-else {
-    xrandr --output DP-0 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-0 --off --output DP-2 --off --output None-2-1 --off --output eDP-1-1 --off --output DP-1-1 --primary --mode 2560x1440 --pos 2560x0 --rotate normal --output DP-1-2 --off --output DP-1-3 --off --output DP-1-4 --off
+    xrandr --output DP-0 --mode 2560x1440 --rate 60 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-0 --off --output DP-2 --off --output None-2-1 --off --output eDP-1-1 --off --output DP-1-1 --primary --mode 2560x1440 --rate 60 --pos 2560x0 --rotate normal --output DP-1-2 --off --output DP-1-3 --off --output DP-1-4 --off
 } fi
+# elif [ -n "$DP_1" ] && [ -n "$eDP_1" ] ; then {
+#     xrandr --output eDP-1 --mode 1920x1200 --pos 480x2160 --rotate normal --rate 165 --output DP-1 --primary --mode 3840x2160 --pos 0x0 --rotate normal --rate 60
+# } 
+# else {
+#     xrandr --output DP-0 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-0 --off --output DP-2 --off --output None-2-1 --off --output eDP-1-1 --off --output DP-1-1 --primary --mode 2560x1440 --pos 2560x0 --rotate normal --output DP-1-2 --off --output DP-1-3 --off --output DP-1-4 --off
+# } fi
 
 xbacklight -set 1
