860cb649b1db355a26e394c60061c60a12f8d70b
Alex Rizzoni <rizzoni.alex@gmail.com>
2024-03-21 10:14:57 -0400

    set background in wal script

--- bin/wal.sh
+++ bin/wal.sh
@@ -19,8 +19,16 @@ LN="command ln -s"
 LS="command ls"
 WAL="command wal"
 
-# Use old cached wallpaper to generate a colorscheme
+# Define cached wallpaper to use
 WAL_CACHE="$CACHE/wal_cache"
+
+# If feh is executable, set the desktop wallpaper
+if [ -x "$(command -v feh)" ] ; then { 
+    FEH="command feh --bg-fill --no-fehbg"
+    $FEH "$($CAT "$WAL_CACHE")"
+} fi
+
+# Generate the colorscheme
 $WAL --saturate 0.5 --cols16 -nq -i "$($CAT "$WAL_CACHE")"
 
 # Select new random wallpaper and cache it
@@ -33,29 +41,30 @@ WALLPAPER_CACHE="/usr/share/backgrounds/wal"
 $CP "$WALLPAPER_DIR/$WALLPAPER" "$WALLPAPER_CACHE"
 
 # Apply colorscheme to Alacritty
-if [ -x "$(command -v alacritty)" ] &&                                       \
-    [ ! -e "$CONFIG/alacritty/alacritty.toml" ] &&                           \
-    [ -e "$CACHE/alacritty.wal" ] ; then {
-        $LN "$CACHE/alacritty.wal" "$CONFIG/alacritty/alacritty.toml"
+if [ -x "$(command -v alacritty)" ] &&                                         \
+    [ ! -e "$CONFIG/alacritty/alacritty_colors.toml" ] &&                      \
+    [ -e "$CACHE/alacritty_colors.toml" ] ; then {
+        $LN "$CACHE/alacritty_colors.toml"                                     \
+            "$CONFIG/alacritty/alacritty_colors.toml"
 } fi
 
 # Apply pywal colorscheme to Zathura 
-if [ -x "$(command -v zathura)" ] &&                                         \
-    [ ! -e "$CONFIG/zathura/zathurarc" ] &&                                  \
+if [ -x "$(command -v zathura)" ] &&                                           \
+    [ ! -e "$CONFIG/zathura/zathurarc" ] &&                                    \
     [ -e "$CACHE/zathurarc.wal" ] ; then { 
         $LN "$CACHE/zathura.wal" "$CONFIG/zathura/zathurarc"
 } fi
 
 # Apply colorscheme to CAVA
-if [ -x "$(command -v cava)" ] &&                                            \
-    [ ! -e "$CONFIG/cava/config" ] &&                                        \
+if [ -x "$(command -v cava)" ] &&                                              \
+    [ ! -e "$CONFIG/cava/config" ] &&                                          \
     [ -e "$CACHE/cava.wal" ] ; then {
         $LN "$CACHE/cava.wal" "$CONFIG/cava/config"
 } fi
 
 # Apply colorscheme to AERC
-if [ -x "$(command -v aerc)" ] &&                                            \
-    [ ! -e "$CONFIG/aerc/stylesets/wal" ] &&                                 \
+if [ -x "$(command -v aerc)" ] &&                                              \
+    [ ! -e "$CONFIG/aerc/stylesets/wal" ] &&                                   \
     [ -e "$CACHE/aerc.wal" ] ; then {
         $LN "$CACHE/aerc.wal" "$CONFIG/aerc/stylesets/wal"
 } fi
@@ -68,4 +77,12 @@ if [ -x "$(command -v pywalfox)" ] ; then {
 # Apply pywal colorscheme to KiCad
 if [ -f ~/Projects/kiwal/kiwal.py ] ; then {
     command python ~/Projects/kiwal/kiwal.py
+} fi
+
+if [ -x "$(command -v xrdb)" ] ; then {
+    xrdb -merge "$CACHE/colors.Xresources"
+} fi
+
+if [ "$XDG_SESSION_DESKTOP" = 'awesome' ] ; then {
+    echo 'awesome.restart()' | awesome-client 1>&2
 } fi
