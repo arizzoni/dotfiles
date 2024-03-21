17b679ab4047bfffca568bf6c90bce2188b92042
Alex Rizzoni <rizzoni.alex@gmail.com>
2024-03-21 10:15:07 -0400

    tuning script

--- bin/monitors.sh
+++ bin/monitors.sh
@@ -8,8 +8,6 @@ eDP_1="$($CONNECTED_MONITORS | grep eDP-1)"
 eDP_1_1="$($CONNECTED_MONITORS | grep eDP-1-1)"
 DP_1_1="$($CONNECTED_MONITORS | grep DP-1-1)"
 
-# CONNECTED_MONITORS="$(xrandr | grep ' connected ' | sed s/\ connected\ .*//g)"
-#
 # for MONITOR in $CONNECTED_MONITORS ; do
 # 	DETAILS="$(xrandr -q | grep "$MONITOR" | sed s/"$MONITOR"//g | tr '\n' ' ' | sed -e 's/[^0-9]/ /g' -e 's/^ *//g' -e 's/ *$//g' | tr -s ' ' | sed 's/ /\n/g' )"
 # 	X_RESOLUTION="$(echo "$DETAILS" | sed -n '1{p;q}')"
@@ -22,15 +20,12 @@ DP_1_1="$($CONNECTED_MONITORS | grep DP-1-1)"
 # 	} fi
 # done
 
-
-if [ -n "$DP_0" ] && [ -n "$DP_1_1" ] && [ -n "$eDP_1_1" ] ; then {
-    xrandr --output DP-0 --mode 2560x1440 --rate 60 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-0 --off --output DP-2 --off --output None-2-1 --off --output eDP-1-1 --off --output DP-1-1 --primary --mode 2560x1440 --rate 60 --pos 2560x0 --rotate normal --output DP-1-2 --off --output DP-1-3 --off --output DP-1-4 --off
-} fi
-# elif [ -n "$DP_1" ] && [ -n "$eDP_1" ] ; then {
-#     xrandr --output eDP-1 --mode 1920x1200 --pos 480x2160 --rotate normal --rate 165 --output DP-1 --primary --mode 3840x2160 --pos 0x0 --rotate normal --rate 60
-# } 
-# else {
-#     xrandr --output DP-0 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-0 --off --output DP-2 --off --output None-2-1 --off --output eDP-1-1 --off --output DP-1-1 --primary --mode 2560x1440 --pos 2560x0 --rotate normal --output DP-1-2 --off --output DP-1-3 --off --output DP-1-4 --off
-# } fi
+if [ -n "$DP_0" ] && [ -n "$DP_1_1" ] && [ -n "$eDP_1_1" ] ; then
+    xrandr --output DP-0 --mode 3840x2160 --rate 60 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-0 --off --output DP-2 --off --output None-2-1 --off --output eDP-1-1 --off --output DP-1-1 --primary --mode 3840x2160 --rate 60 --pos 3840x0 --rotate normal --output DP-1-2 --off --output DP-1-3 --off --output DP-1-4 --off
+else
+    for MONITOR in $CONNECTED_MONITORS ; do 
+        xrandr --output "$MONITOR" --auto
+    done
+fi
 
 xbacklight -set 1
