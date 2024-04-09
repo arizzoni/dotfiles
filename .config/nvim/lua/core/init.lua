6ab809c19d7e5ec363f9a0ba2aedfbb1cb2192f2
Alex Rizzoni <rizzoni.alex@gmail.com>
2024-04-09 10:55:32 -0400

    cleaned up python host program

--- .config/nvim/lua/core/init.lua
+++ .config/nvim/lua/core/init.lua
@@ -4,10 +4,23 @@
 vim.opt.updatetime = 250     -- Decrease update time
 vim.g.transparent_enabled = true
 vim.opt.termguicolors = true -- Make sure the terminal supports this
+vim.opt.title = true
 
 -- Backup and undo
 vim.opt.writebackup = true
 vim.opt.undofile = true -- Save undo history
+
+-- Neovide Settings
+if vim.g.neovide then
+  vim.g.neovide_transparency = 0.8
+  vim.g.neovide_background_color = "#000000"
+  vim.g.neovide_confirm_quit = true
+  vim.g.neovide_cursor_animation_length = 0.125
+  vim.g.neovide_cursor_trail_size = 0.5
+  vim.g.neovide_cursor_antialiasing = true
+  vim.g.neovide_cursor_animate_command_line = true
+  vim.g.neovide_cursor_vfx_mode = ""
+end
 
 -- Searching
 vim.opt.ignorecase = true -- Case insensitive searching UNLESS /C or capital in search
@@ -19,7 +32,7 @@ vim.opt.timeoutlen = 300
 
 -- Language support
 vim.g.shell = "bash"
-vim.g.python3_host_prog = "/home/air/.local/share/virtualenvs/neovim/bin/python" -- Python executable
+vim.g.python3_host_prog = vim.fn.expand("~/.local/share/virtualenvs/neovim/bin/python") -- Python executable
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_node_provider = 0
