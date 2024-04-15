0176c8f40349c60869209f981c525b095c1cee94
Alex Rizzoni <rizzoni.alex@gmail.com>
2024-04-15 17:27:47 -0400

    removed autoclose on bufenter so iron will work properly

--- .config/nvim/lua/core/terminal/init.lua
+++ .config/nvim/lua/core/terminal/init.lua
@@ -1,30 +1,18 @@
 --[[ Terminal Settings ]]
 
 -- Start terminal in insert mode
--- TODO: get colored prompt working in neovide
 local term_enter_group = vim.api.nvim_create_augroup("TerminalEnter", { clear = true })
 vim.api.nvim_create_autocmd({ "TermOpen", "BufEnter" }, {
   pattern = { "*" },
   group = term_enter_group,
   callback = function()
     if vim.opt.buftype:get() == "terminal" then
-      vim.cmd("startinsert")
       vim.o.number = false
       vim.o.relativenumber = false
     end
   end
 })
 
--- Close terminal when shell exits
-local term_exit_group = vim.api.nvim_create_augroup("TerminalExit", { clear = true })
-vim.api.nvim_create_autocmd("TermClose", {
-  group = term_exit_group,
-  callback = function()
-    vim.cmd("bdelete")
-  end
-})
-
-vim.keymap.set("n", "<leader>tt", function()
+vim.keymap.set("n", "<leader>T", function()
   vim.cmd(":term")
-end, { desc = "Launch Terminal ([T]ele[t]ype)" })
-
+end, { desc = "Launch [T]erminal" })
