1045279e4c938192ce3911e09fef6a67db826aaa
Alex Rizzoni <rizzoni.alex@gmail.com>
2024-04-15 17:21:23 -0400

    improved cmp setup

--- .config/nvim/lua/plugins/nvim-cmp/init.lua
+++ .config/nvim/lua/plugins/nvim-cmp/init.lua
@@ -4,26 +4,77 @@ return {
   name = "nvim-cmp",
   dependencies = {
     -- Snippet Engine & its associated nvim-cmp source
-    "L3MON4D3/LuaSnip",         -- Snippet Engine
-    "saadparwaiz1/cmp_luasnip", -- Luasnip source
+    "L3MON4D3/LuaSnip",                     -- Snippet Engine
+    "saadparwaiz1/cmp_luasnip",             -- Luasnip source
     -- Adds LSP completion capabilities
-    "hrsh7th/cmp-nvim-lsp",     -- LSP completion
-    "hrsh7th/cmp-buffer",       -- Buffer completion
-    "hrsh7th/cmp-path",         -- Completion for paths
-    "hrsh7th/cmp-cmdline",      -- Completion for command line
-    "hrsh7th/cmp-nvim-lua",     -- Neovim Lua completion source
-    "ray-x/cmp-treesitter",     -- Treesitter Autocompletion
-    "petertriho/cmp-git",       -- Adds git autocompletion
-    "lukas-reineke/cmp-rg",     -- Ripgrep autocompletion
-    "kdheepak/cmp-latex-symbols",     -- LaTeX symbol autocompletion
+    "hrsh7th/cmp-nvim-lsp",                 -- LSP completion
+    "hrsh7th/cmp-nvim-lsp-document-symbol", -- LSP completion
+    "hrsh7th/cmp-nvim-lsp-signature-help",  -- LSP completion
+    "hrsh7th/cmp-buffer",                   -- Buffer completion
+    "hrsh7th/cmp-calc",                     -- Math completion
+    "hrsh7th/cmp-path",                     -- Completion for paths
+    "hrsh7th/cmp-cmdline",                  -- Completion for command line
+    "hrsh7th/cmp-nvim-lua",                 -- Neovim Lua completion source
+    "rcarriga/cmp-dap",                     -- DAP completion source
+    "micangl/cmp-vimtex",                   -- Vimtex completion source
+    "ray-x/cmp-treesitter",                 -- Treesitter Autocompletion
+    "petertriho/cmp-git",                   -- Adds git autocompletion
+    "lukas-reineke/cmp-rg",                 -- Ripgrep autocompletion
+    "kdheepak/cmp-latex-symbols",           -- LaTeX symbol autocompletion
+    "windwp/nvim-autopairs",                -- Autopairs completion
   },
   opts = function()
     local cmp = require "cmp"
     local luasnip = require "luasnip"
     require("luasnip.loaders.from_vscode").lazy_load()
     luasnip.config.setup {}
+    local kind_icons = {
+      Text = "",
+      Method = "󰆧",
+      Function = "󰊕",
+      Constructor = "",
+      Field = "󰇽",
+      Variable = "󰂡",
+      Class = "󰠱",
+      Interface = "",
+      Module = "",
+      Property = "󰜢",
+      Unit = "",
+      Value = "󰎠",
+      Enum = "",
+      Keyword = "󰌋",
+      Snippet = "",
+      Color = "󰏘",
+      File = "󰈙",
+      Reference = "",
+      Folder = "󰉋",
+      EnumMember = "",
+      Constant = "󰏿",
+      Struct = "",
+      Event = "",
+      Operator = "󰆕",
+      TypeParameter = "󰅲",
+    }
 
     return {
+      view = {
+        entries = "custom"
+      },
+      window = {
+        completion = {
+          winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+          col_offset = -3,
+          side_padding = 0,
+        },
+      },
+      formatting = {
+        fields = { "kind", "abbr", "menu" },
+        format = function(_, vim_item)
+          vim_item.menu = string.format('(%s)', vim_item.kind)
+          vim_item.kind = string.format(' %s ', kind_icons[vim_item.kind])
+          return vim_item
+        end
+      },
       snippet = {
         expand = function(args)
           luasnip.lsp_expand(args.body)
@@ -64,6 +115,15 @@ return {
       },
       sources = {
         { name = "nvim_lsp" },
+        { name = "cmp-vimtex" },
+        { name = "nvim_lua" },
+        { name = "nvim_lsp_document_symbol" },
+        { name = "nvim_lsp_signature_help" },
+        { name = "path" },
+        { name = "dap" },
+        { name = "calc" },
+        { name = "cmdline" },
+        { name = "treesitter" },
         { name = "luasnip" },
         { name = "buffer" },
         { name = "git" },
@@ -80,5 +140,14 @@ return {
         end
       end
     }
+  end,
+  init = function()
+    -- If you want insert `(` after select function or method item
+    local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+    local cmp = require('cmp')
+    cmp.event:on(
+      'confirm_done',
+      cmp_autopairs.on_confirm_done()
+    )
   end
 }
