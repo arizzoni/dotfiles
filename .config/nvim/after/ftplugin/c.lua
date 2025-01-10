54abfc143734b323da2f5ff07a9f37948ab50474
Alex Rizzoni <rizzoni.alex@gmail.com>
2025-01-10 12:25:29 -0500

    got ccls working

--- .config/nvim/after/ftplugin/c.lua
+++ .config/nvim/after/ftplugin/c.lua
@@ -7,13 +7,33 @@ vim.bo.softtabstop = vim.bo.tabstop
 local bufnr = vim.api.nvim_get_current_buf()
 
 term.new()
-
+local function switch_source_header(bufnr)
+	local method_name = "textDocument/switchSourceHeader"
+	bufnr = util.validate_bufnr(bufnr)
+	local client = util.get_active_client_by_name(bufnr, "ccls")
+	if not client then
+		return vim.notify(
+			("method %s is not supported by any servers active on the current buffer"):format(method_name)
+		)
+	end
+	local params = vim.lsp.util.make_text_document_params(bufnr)
+	client.request(method_name, params, function(err, result)
+		if err then
+			error(tostring(err))
+		end
+		if not result then
+			vim.notify("corresponding file cannot be determined")
+			return
+		end
+		vim.cmd.edit(vim.uri_to_fname(result))
+	end, bufnr)
+end
 local root_dir = vim.fs.dirname(vim.fs.find({
 	"compile_commands.json",
 	"compile_flags.txt",
 	"configure.ac", -- AutoTools
 	".git",
-}, { upward = true })[1])
+}, { upward = true })[1]) or vim.fn.getcwd()
 
 local ccls = vim.lsp.start({
 	name = "ccls",
@@ -25,12 +45,11 @@ local ccls = vim.lsp.start({
 		index = {
 			threads = 0,
 		},
-		cache = { directory = ".ccls-cache" },
+		cache = { directory = vim.env.XDG_CACHE_HOME .. "ccls/cache" },
 		clang = {
 			excludeArgs = { "-frounding-math" },
 		},
 	},
-
 	capabilities = {
 		textDocument = {
 			completion = {
@@ -38,6 +57,14 @@ local ccls = vim.lsp.start({
 			},
 		},
 		offsetEncoding = { "utf-32" },
+	},
+	commands = {
+		CclsSwitchSourceHeader = {
+			function()
+				switch_source_header(0)
+			end,
+			description = "Switch between source/header",
+		},
 	},
 	docs = {
 		description = { "CCLS" },
